cf766b305317b9c195c5f41add79126601bd34c9
lalitha <18pa1a0567@vishnu.edu.in>
2020-10-17 20:54:58 +0530

    Updates

---
+++ precache-manifest.7351aa35397574d352777d70a51f7744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c42ca89a3cc80256305a600fd1f616d",
+    "url": "/react-app-todolist/index.html"
+  },
+  {
+    "revision": "cf5038664255969a3b04",
+    "url": "/react-app-todolist/static/css/main.4e0cd263.chunk.css"
+  },
+  {
+    "revision": "173034364ad858a1470f",
+    "url": "/react-app-todolist/static/js/2.8c0e1eac.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/react-app-todolist/static/js/2.8c0e1eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5038664255969a3b04",
+    "url": "/react-app-todolist/static/js/main.b4cf8538.chunk.js"
+  },
+  {
+    "revision": "03d60d4d6e099485236d",
+    "url": "/react-app-todolist/static/js/runtime-main.43de49e2.js"
+  }
+]);
